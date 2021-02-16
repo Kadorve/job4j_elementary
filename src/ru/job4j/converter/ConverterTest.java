b85ca649bab40065fcf18c81b1118870b4c7c04c
Kadorve <protriarii@mail.ru>
2021-02-16 22:46:24 +0500

    1. Модульные тесты. [#472429]

---
+++ src/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.converter;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest extends TestCase {
+
+    public void testRubleToEuro() {
+        int in1 = 140;
+        int expected1 = 2;
+        int out1 = Converter.rubleToEuro(in1);
+        Assert.assertEquals(expected1, out1);
+    }
+
+    public void testRubleToDollar() {
+        int in2 = 300;
+        int expected2 = 5;
+        int out2 = Converter.rubleToDollar(in2);
+        Assert.assertEquals(expected2, out2);
+    }
+}
