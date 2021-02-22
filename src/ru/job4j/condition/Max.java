a03a58981db14f3c8da4d396b8b29f9b8cc33a1a
Kadorve <protriarii@mail.ru>
2021-02-22 14:25:15 +0500

    4.2. Тернарное сравнение [#472356]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -7,7 +7,7 @@ public class Max {
     }
 
     public static void main(String[] args) {
-        int result = Max.max(3, 3);
+        int result = Max.max(2, 2);
         System.out.println(result);
     }
 }
